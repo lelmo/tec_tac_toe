271b8c08e64116df371b50a1543b062c345e2ea7
lelmo <55011413+lelmo@users.noreply.github.com>
2019-09-30 18:58:25 +0300

    tictactoe

--- src/index.js
+++ src/index.js
@@ -15,27 +15,150 @@ const showAlert = num => {
   let item = document.getElementById(num);
   item.innerHTML = player;
 
-  console.log(item);
+  //  console.log(item);
   console.log(num + " pressed");
 
   tic_tac_toe[~~(num / 5)][num % 5] = player;
   // console.log(tic_tac_toe[1]);
 
+  check_win();
   if (player === "X") player = "O";
   else player = "X";
 
   let new_player_text = document.getElementById("text");
   new_player_text.innerHTML = "player " + player + "'s turn";
 
-  console.log(player);
+  // console.log(player);
+};
+
+const check_win = () => {
+  for (let i = 0; i < 3; i++) {
+    //Check for x axis
+    for (let j = 0; j < 3; j++) {
+      if (
+        tic_tac_toe[i][j] === "X" &&
+        tic_tac_toe[i][j + 1] === "X" &&
+        tic_tac_toe[i][j + 2] === "X"
+      ) {
+        console.log("X wins");
+        wictory();
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (
+        tic_tac_toe[i][j] === "O" &&
+        tic_tac_toe[i][j + 1] === "O" &&
+        tic_tac_toe[i][j + 2] === "O"
+      ) {
+        console.log("O wins");
+        wictory();
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    // check for y axis
+    for (let j = 0; j < 3; j++) {
+      if (
+        tic_tac_toe[i][j] === "X" &&
+        tic_tac_toe[i + 1][j] === "X" &&
+        tic_tac_toe[i + 2][j] === "X"
+      ) {
+        console.log("X wins");
+        wictory();
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (
+        tic_tac_toe[i][j] === "O" &&
+        tic_tac_toe[i + 1][j] === "O" &&
+        tic_tac_toe[i + 2][j] === "O"
+      ) {
+        console.log("O wins");
+        wictory();
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    // check for diagonal to right
+    for (let j = 0; j < 3; j++) {
+      if (
+        tic_tac_toe[i][j] === "X" &&
+        tic_tac_toe[i + 1][j + 1] === "X" &&
+        tic_tac_toe[i + 2][j + 2] === "X"
+      ) {
+        console.log("X wins");
+        wictory();
+        return;
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (
+        tic_tac_toe[i][j] === "O" &&
+        tic_tac_toe[i + 1][j + 1] === "O" &&
+        tic_tac_toe[i + 2][j + 2] === "O"
+      ) {
+        console.log("O wins");
+        wictory();
+        return;
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    // check for diagonal to left
+    for (let j = 4; j > 1; j--) {
+      if (
+        tic_tac_toe[i][j] === "X" &&
+        tic_tac_toe[i + 1][j - 1] === "X" &&
+        tic_tac_toe[i + 2][j - 2] === "X"
+      ) {
+        console.log("X wins");
+        wictory();
+        return;
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 4; j > 1; j--) {
+      if (
+        tic_tac_toe[i][j] === "O" &&
+        tic_tac_toe[i + 1][j - 1] === "O" &&
+        tic_tac_toe[i + 2][j - 2] === "O"
+      ) {
+        console.log("O wins");
+        wictory();
+        return;
+      }
+    }
+  }
+};
+
+const wictory = () => {
+  let win = document.createTextNode("Wictory!");
+  wictory_text.appendChild(win);
 };
 
 let table_div = document.getElementById("board");
 let player_text = document.createElement("d");
+let wictory_text = document.createElement("d");
 let text = document.createTextNode("player " + player + "'s turn");
 player_text.appendChild(text);
 player_text.setAttribute("id", "text");
+wictory_text.setAttribute("id", "won");
 table_div.appendChild(player_text);
+table_div.appendChild(wictory_text);
 
 let table = document.createElement("table");
 
